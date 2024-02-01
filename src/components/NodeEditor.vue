909bbf70964cc1d75a33d429adbcc1e200f3bab9
Malthusis <58402624+Malthusis@users.noreply.github.com>
2024-02-01 00:48:52 -0600

    Added some basic styling and fixed a few minor things.

--- src/components/NodeEditor.vue
+++ src/components/NodeEditor.vue
@@ -1,41 +1,54 @@
 <template>
 <div class="formPanel">
-    ID: {{ selectedNode.id }}<br/>
-    Type: {{ selectedNode.type }}<br/>
-    Position: ({{ selectedNode.position?.x }}, {{ selectedNode.position?.y }})<br/>
-    <hr class="solid">
-    ==Current Data==<br/>
-    Area Special ID: {{ selectedNode.data.areaSpecialID }}<br/>
-    <!--TODO: The text here is absolutely fucked for some reason but thats an issue for later -->
-    Custom Function: <pre>{{ selectedNode.data.customFunc }}</pre><br/>
-    Name: {{ selectedNode.data.name }}<br/>
-    Zone: {{ selectedNode.data.zone }}<br/>
-    Description: {{ selectedNode.data.description }}<br/>
-    Killcount: {{ selectedNode.data.killCount }}<br/>
-    Scout Threshold: {{ selectedNode.data.scoutThreshold }}<br/>
-    interactable: {{ selectedNode.data.interactable }}<br/>
-    <hr class="solid">
-    ==New data==<br/>
-    Area Special ID: <input v-model="nodeDataUpdate.data.areaSpecialID"/><br/>
-    <!--TODO: textareas appear ABOVE line intsead of hanging below it, idk how to fix that but it doesnt matter rn -->
-    Custom Function: <textarea v-model="nodeDataUpdate.data.customFunc" ></textarea><br/>
-    Name: <input v-model="nodeDataUpdate.data.areaName"/><br/>
-    Zone: <input v-model="nodeDataUpdate.data.zone" /><br/>
-    Description: <textarea v-model="nodeDataUpdate.data.description" ></textarea><br/>
-    Killcount: <input v-model="nodeDataUpdate.data.killCount"/><br/>
-    Scout Threshold: <input v-model="nodeDataUpdate.data.scoutThreshold"/><br/>
-    interactable: <input type="checkbox" id="checkbox" v-model="nodeDataUpdate.data.interactable" />
-    <label for="checkbox">{{ nodeDataUpdate.data.interactable }}</label><br/><br/>
+    <div class="row-box coords">
+        <span>ID: {{ selectedNode.id }}</span>
+        <span>Type: {{ selectedNode.type }}<br/></span>
+        <span>Position: ({{ selectedNode.position?.x }}, {{ selectedNode.position?.y }})</span>
+    </div>
+    <div class="divider"></div>
+    <span class="title">==Current Data==</span>
+    <div class="currentData">
+        Area Special ID: {{ selectedNode.data.areaSpecialID }}<br/>
+        <!--TODO: The text here is absolutely fucked for some reason but thats an issue for later -->
+        Custom Function: <pre>{{ selectedNode.data.customFunc }}</pre><br/>
+        Name: {{ selectedNode.data.areaName }}<br/>
+        Zone: {{ selectedNode.data.zone }}<br/>
+        Description: {{ selectedNode.data.description }}<br/>
+        <div class="row-box">
+            <span>Killcount: {{ selectedNode.data.killCount }}</span>
+            <span>Scout Threshold: {{ selectedNode.data.scoutThreshold }}</span>
+            <span>interactable: {{ selectedNode.data.interactable }}</span>
+        </div>    
+    </div>
+    <div class="divider"></div>
+    <span class="title">==New Data==</span>
+    <div class="newData">
+        Area Special ID: <input v-model="nodeDataUpdate.data.areaSpecialID"/><br/>
+        <!--TODO: textareas appear ABOVE line intsead of hanging below it, idk how to fix that but it doesnt matter rn -->
+        Custom Function: <textarea v-model="nodeDataUpdate.data.customFunc" ></textarea><br/>
+        Name: <input v-model="nodeDataUpdate.data.areaName" placeholder="Node name"/><br/>
+        Zone: <select v-model="nodeDataUpdate.data.zone"> 
+            <option v-for="(item) in zoneOptions" :value="item" >{{ item }}</option>
+        </select><br/>
+        Description: <textarea v-model="nodeDataUpdate.data.description" placeholder="insert node description here"></textarea><br/>
+        Killcount: <input v-model="nodeDataUpdate.data.killCount"/><br/>
+        Scout Threshold: <input v-model="nodeDataUpdate.data.scoutThreshold"/><br/>
+        interactable: <input type="checkbox" id="checkbox" v-model="nodeDataUpdate.data.interactable" />
+        <label for="checkbox">{{ nodeDataUpdate.data.interactable }}</label><br/><br/>
+    </div>
     <button class="updateButton" @click="updateNode()">Update</button>
+    
 </div>
 </template>
 
 <script setup lang="ts">
     import { useMapStore } from '@/stores/mapStore.js';
+    import { Zone } from '@/types/areaData';
     import { useVueFlow, type GraphNode } from '@vue-flow/core';
     import { ref, toRefs, watch } from 'vue';
 
     const mapStore = useMapStore();
+    const zoneOptions = [Zone.FOREST, Zone.DEEP_FOREST, Zone.SPECIAL]
 
     const { nodesDraggable, onPaneReady, elementsSelectable, onNodeClick,  findNode, addEdges, nodes, edgesUpdatable, nodesConnectable, setMinZoom, onConnect, addNodes, onPaneClick, project, vueFlowRef, selectionKeyCode, onEdgeClick } = useVueFlow({ id:"map"});
 
@@ -45,7 +58,7 @@
             areaSpecialID: "",
             customFunc: "",
             areaName: "",
-            zone: "",
+            zone: Zone.FOREST,
             description: "",
             killCount: 0,
             scoutThreshold: 0,
@@ -85,5 +98,31 @@ hr.solid {
 }
 .formPanel {
     background-color: aliceblue;
+}
+.coords {
+    background-color: rgb(61, 190, 255);
+}
+.row-box {
+    display:flex;
+    justify-content:space-between;
+    span {
+        margin: 0 8px;
+    }
+}
+.currentData {
+    background-color:rgb(172, 255, 124);
+}
+.newData {
+    background-color:rgb(253, 160, 160);
+}
+.row {
+    display:flex;
+}
+.divider {
+    border-bottom: 6px solid;
+}
+.title { 
+    display: flex; 
+    justify-content: center 
 }
 </style>
--- src/types/areaData.ts
+++ src/types/areaData.ts
@@ -1,12 +1,21 @@
-
-
 export interface AreaData {
     areaSpecialID?: string;
     customFunc?: string;
     areaName: string;
-    zone: string;
+    zone: Zone;
     description: string;
     killCount: number;
     scoutThreshold: number;
     interactable: false;
+}
+
+export enum SpecialAreaId {
+    HOME = "Home",
+    CLEARING = "Clearing"
+}
+
+export enum Zone {
+    SPECIAL = "special",
+    FOREST = "forest",
+    DEEP_FOREST = "deep_forest"
 }
